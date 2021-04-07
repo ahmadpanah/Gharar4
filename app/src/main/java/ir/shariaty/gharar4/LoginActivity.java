b9c020242ab92a00bcd437cda685421d78e702ac
Hossein <djalicrt@gmail.com>
2021-04-07 18:58:41 +0430

    Finish Signup Activity Logic

--- app/src/main/java/ir/shariaty/gharar4/LoginActivity.java
+++ app/src/main/java/ir/shariaty/gharar4/LoginActivity.java
@@ -2,13 +2,33 @@ package ir.shariaty.gharar4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText emailBox, passwordBox;
+    Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailBox = findViewById(R.id.emailBox);
+        passwordBox = findViewById(R.id.passwordBox);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signupBtn = findViewById(R.id.createBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/ir/shariaty/gharar4/SignupActivity.java
+++ app/src/main/java/ir/shariaty/gharar4/SignupActivity.java
@@ -1,14 +1,59 @@
 package ir.shariaty.gharar4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    EditText emailBox, passwordBox ,nameBox;
+    Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+
+        emailBox = findViewById(R.id.emailBox);
+        passwordBox = findViewById(R.id.passwordBox);
+        nameBox = findViewById(R.id.nameBox);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signupBtn = findViewById(R.id.createBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, pass, name;
+                email = emailBox.getText().toString();
+                pass = passwordBox.getText().toString();
+                name = nameBox.getText().toString();
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Your Account is Created!",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
