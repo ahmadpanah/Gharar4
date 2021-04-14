dbcb49375eee711ea25d096310ef45167ea28775
Hossein <djalicrt@gmail.com>
2021-04-14 18:10:17 +0430

    Login - Signup - DashboardActivity Logic is Done! Integrated with Jitsi SDK!

--- app/src/main/java/ir/shariaty/gharar4/SignupActivity.java
+++ app/src/main/java/ir/shariaty/gharar4/SignupActivity.java
@@ -3,6 +3,7 @@ package ir.shariaty.gharar4;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +11,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -20,11 +23,14 @@ public class SignupActivity extends AppCompatActivity {
     EditText emailBox, passwordBox ,nameBox;
     Button loginBtn, signupBtn;
 
+    FirebaseFirestore database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        database = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
 
         emailBox = findViewById(R.id.emailBox);
@@ -42,10 +48,22 @@ public class SignupActivity extends AppCompatActivity {
                 pass = passwordBox.getText().toString();
                 name = nameBox.getText().toString();
 
+                User user = new User();
+                user.setEmail(email);
+                user.setPass(pass);
+                user.setName(name);
+
                 auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            database.collection("Users")
+                                    .document().set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+                                }
+                            });
                             Toast.makeText(SignupActivity.this, "Your Account is Created!",Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
